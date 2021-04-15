4e7187624ec6f8f1293d646d8b319a2f1084e47d
Putu Wisnu Ardia Chandra <wisnu.ardia@undiksha.ac.id>
2021-04-15 19:34:57 +0800

    Putu Wisnu Ardia Chandra

---
+++ lib/round_icon_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Function onPressed;
+  RoundIconButton({@required this.icon, @required this.onPressed});
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      elevation: 0.0,
+      child: Icon(
+        icon,
+        size: 30,
+        color: Colors.white,
+      ),
+      onPressed: onPressed,
+      constraints: BoxConstraints.tightFor(
+        width: 56,
+        height: 56.0,
+      ),
+      fillColor: Color(0xFF4C4F5E),
+      shape: CircleBorder(),
+    );
+  }
+}
